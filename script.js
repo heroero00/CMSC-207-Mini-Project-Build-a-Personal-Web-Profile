335227a374cba6d87a5b801e1b0c1de99fddbc06
heroero00 <31796355+heroero00@users.noreply.github.com>
2026-03-15 23:11:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function showFunFact() {
+    const fact = "Fun Fact: This is one of the first thing I learned in javaScript in college";
+    document.getElementById("funFact").textContent = fact;
+}
